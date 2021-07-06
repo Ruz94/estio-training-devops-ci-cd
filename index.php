ba5947c21222b0c855352f0dd0e30f7aa12fdb88
Ruzina Begum <ruzina75@gmail.com>
2021-07-06 14:48:25 +0100

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+<section class="videoWrapper">
+      <div class="iframeContainer">
+        <iframe
+          src="https://www.youtube.com/embed/NwzJCSPSPZs"
+          width="560"
+          height="315"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </section>
+
+</body>
+</html>
